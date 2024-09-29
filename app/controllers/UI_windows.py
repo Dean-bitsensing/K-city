3be8e10d677ab3737bef214ff5afcd824739f6b3
kenneth <kenneth@bitsensing.com>
2024-09-29 16:14:07 +0900

    update UI window

--- app/controllers/UI_windows.py
+++ app/controllers/UI_windows.py
@@ -7,6 +7,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
 from tkinter import simpledialog
 
+
 class DataProcessingThread(threading.Thread):
     def __init__(self, config, atm, result_queue, start_date, day):
         super().__init__()
@@ -19,7 +20,7 @@ class DataProcessingThread(threading.Thread):
     def run(self):
         df, date_range = self.load_and_merge_csv_files()
         if not df.empty:
-            self.atm.vds_view = True  # 데이터가 처리된 경우 True로 설정
+            self.atm.vds_view = True
             self.result_queue.put((self.atm, df, date_range))
 
     def load_and_merge_csv_files(self):
@@ -34,35 +35,31 @@ class DataProcessingThread(threading.Thread):
             try:
                 all_files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.csv')]
                 if not all_files:
-                    print(f"No CSV files found in directory: {directory}")  # 로그 메시지
-                    continue  # 파일이 없으면 넘어감
+                    print(f"No CSV files found in directory: {directory}")
+                    continue
                 
                 for file in all_files:
                     df = pd.read_csv(file)
                     df['time'] = pd.to_datetime(df['time'], format='%Y%m%d_%H:%M:%S')
-                    df['time'] = df['time'] - pd.Timedelta(hours=7)
                     df['hour_bin'] = df['time'].dt.hour
+                    df['weekday'] = df['time'].dt.weekday
+
+                    df['count'] = 1
+
                     if not df.empty and df.notna().sum().sum() > 0:
                         df_list.append(df)
             except FileNotFoundError:
-                print(f"Directory not found: {directory}")  # 디렉토리가 없을 경우 로그 메시지
+                print(f"Directory not found: {directory}")
                 continue
 
-        date_range = (min(date_range), max(date_range))  # 시작일과 종료일을 구함
+        date_range = (min(date_range), max(date_range))
 
         if df_list:
             merged_df = pd.concat(df_list, ignore_index=True)
-
-            # outgoing과 oncoming을 시간대별로 count하고 레인별로 그룹화
-            lane_groups = merged_df.groupby(['lane', 'direction', 'hour_bin']).size().unstack(fill_value=0)
-
-            # 레인별 평균 계산
-            lane_averages = lane_groups / self.day
-            lane_averages = lane_averages.stack().reset_index(name='count')
-
-            return lane_averages, date_range
+            return merged_df, date_range
         else:
-            return pd.DataFrame(), date_range  # 유효한 데이터가 없으면 빈 데이터프레임 반환
+            return pd.DataFrame(), date_range
+
 
 class PlotAppThread(threading.Thread):
     def __init__(self, config, atm, result_queue):
@@ -70,14 +67,13 @@ class PlotAppThread(threading.Thread):
         self.config = config
         self.atm = atm
         self.result_queue = result_queue
-        self.lane_data = {}  # 레인별 데이터를 저장
-        self.active_lanes = {}  # 활성화된 레인의 리스트
-        self.plot_type = 'bar'  # 그래프 유형 ('bar' 또는 'line')
-        self.date_range = None  # 날짜 범위를 저장할 변수
+        self.lane_data = {}
+        self.active_lanes = {}
+        self.date_range = None
 
     def run(self):
         self.root = tk.Tk()
-        self.root.withdraw()  # Tkinter 창 숨기기
+        self.root.withdraw()
         self.check_queue()
 
         self.root.mainloop()
@@ -87,114 +83,125 @@ class PlotAppThread(threading.Thread):
             while not self.result_queue.empty():
                 atm, df, date_range = self.result_queue.get_nowait()
                 if not df.empty:
-                    self.date_range = date_range  # 날짜 범위 저장
+                    self.date_range = date_range
                     self.process_lane_data(df)
                     self.create_total_lane_plot_window(atm)
             if self.root.winfo_exists():
-                self.root.after(100, self.check_queue)  # 100ms 후 다시 체크
+                self.root.after(100, self.check_queue)
         except tk.TclError:
             pass
 
     def process_lane_data(self, df):
-        # 각 레인별로 데이터를 분류하고 저장
+        self.weekend_data = df[df['weekday'] >= 5]  # Saturday (5) and Sunday (6)
+        self.weekday_data = df[df['weekday'] < 5]   # Monday (0) to Friday (4)
+
+        self.lane_data['weekend'] = {}
+        self.lane_data['weekday'] = {}
+
         for (lane, direction), group_df in df.groupby(['lane', 'direction']):
             key = (str(lane), direction)
-            self.lane_data[key] = group_df
-            self.active_lanes[key] = True  # 모든 레인을 초기화 시 활성화
+            self.active_lanes[key] = True
+
+        for dataset_name, dataset in {'weekend': self.weekend_data, 'weekday': self.weekday_data}.items():
+            for (lane, direction), group_df in dataset.groupby(['lane', 'direction']):
+                key = (str(lane), direction)
+                self.lane_data[dataset_name][key] = group_df
 
     def create_total_lane_plot_window(self, atm):
         plot_window = tk.Toplevel(self.root)
         plot_window.title(f'ATM: {atm.ip}')
-        plot_window.geometry('800x600')
 
-        fig, axs = plt.subplots(2, 1, figsize=(10, 8))
-        self.axs = axs
+        plot_window.geometry('1600x1200')
+        fig, axs = plt.subplots(2, 2, figsize=(16, 12))
+
+        self.axs = axs.flatten()
         self.atm = atm
         self.fig = fig
 
-        # 간격 조정 (subplots 간의 간격을 조정)
-        plt.subplots_adjust(hspace=0.4)  # 상하 간격 조정
+        plt.subplots_adjust(hspace=0.4, wspace=0.4)
 
-        # 그래프 그리기
         self.update_graphs()
 
         canvas = FigureCanvasTkAgg(fig, master=plot_window)
         canvas.draw()
         canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
 
-        # 각 창에서 개별적으로 키보드 이벤트를 처리하도록 설정
         plot_window.bind('<Key>', self.on_key_press)
         plot_window.protocol("WM_DELETE_WINDOW", self.on_close)
 
     def on_key_press(self, event):
-        if event.char.isdigit():  # 숫자 키 입력 처리
+        if event.char.isdigit():
             self.toggle_lane(event.char)
-        elif event.char.lower() == 'r':
-            self.toggle_plot_type()
 
     def toggle_lane(self, lane):
         for direction in ['oncoming', 'outgoing']:
             key = (str(lane), direction)
             if key in self.active_lanes:
-                # 활성화된 상태라면 비활성화
                 self.active_lanes[key] = not self.active_lanes[key]
         self.update_graphs()
 
-    def toggle_plot_type(self):
-        self.plot_type = 'line' if self.plot_type == 'bar' else 'bar'
-        self.update_graphs()
-
     def update_graphs(self):
-        self.axs[0].clear()
-        self.axs[1].clear()
+        for ax in self.axs:
+            ax.clear()
 
         hour_bins = range(24)
         x_ticks = [f"{i:02d}:00" for i in range(24)]
 
-        # 활성화된 모든 레인 데이터를 합쳐서 그래프 그리기
-        for (lane, direction), is_active in self.active_lanes.items():
-            if not is_active:
-                continue
+        # Plot weekend oncoming data
+        self.plot_lane_data(self.lane_data['weekend'], 'oncoming', self.axs[0], 'Weekend Oncoming', hour_bins, x_ticks)
 
-            lane_df = self.lane_data[(lane, direction)]
-            counts = lane_df.groupby('hour_bin')['count'].sum()
+        # Plot weekend outgoing data
+        self.plot_lane_data(self.lane_data['weekend'], 'outgoing', self.axs[1], 'Weekend Outgoing', hour_bins, x_ticks)
 
-            if direction == 'outgoing':
-                if self.plot_type == 'bar':
-                    self.axs[0].bar(counts.index, counts, label=f'Outgoing (Lane {lane})')
-                elif self.plot_type == 'line':
-                    self.axs[0].plot(counts.index, counts, label=f'Outgoing (Lane {lane})')
-            elif direction == 'oncoming':
-                if self.plot_type == 'bar':
-                    self.axs[1].bar(counts.index, counts, label=f'Oncoming (Lane {lane})')
-                elif self.plot_type == 'line':
-                    self.axs[1].plot(counts.index, counts, label=f'Oncoming (Lane {lane})')
+        # Plot weekday oncoming data
+        self.plot_lane_data(self.lane_data['weekday'], 'oncoming', self.axs[2], 'Weekday Oncoming', hour_bins, x_ticks)
 
-        # 그래프 제목에 날짜 범위 추가
-        start_date, end_date = self.date_range
-        self.axs[0].set_title(f'Outgoing Vehicles by Hour\n({start_date} to {end_date})')
-        self.axs[0].set_xlabel('Time')
-        self.axs[0].set_ylabel('Number of Vehicles')
-        self.axs[0].grid(True)
-        self.axs[0].set_xticks(hour_bins)
-        self.axs[0].set_xticklabels(x_ticks, rotation=45)
-        self.axs[0].legend()
+        # Plot weekday outgoing data
+        self.plot_lane_data(self.lane_data['weekday'], 'outgoing', self.axs[3], 'Weekday Outgoing', hour_bins, x_ticks)
 
-        self.axs[1].set_title(f'Oncoming Vehicles by Hour\n({start_date} to {end_date})')
-        self.axs[1].set_xlabel('Time')
-        self.axs[1].set_ylabel('Number of Vehicles')
-        self.axs[1].grid(True)
-        self.axs[1].set_xticks(hour_bins)
-        self.axs[1].set_xticklabels(x_ticks, rotation=45)
-        self.axs[1].legend()
-
-        # 레이아웃 조정을 통해 겹침 방지
         plt.tight_layout()
         self.fig.canvas.draw()
+
+    def plot_lane_data(self, lane_data, direction, ax, title, hour_bins, x_ticks):
+        total_data = pd.Series(0, index=hour_bins)
+
+        for (lane, lane_direction), is_active in self.active_lanes.items():
+            if lane_direction == direction and is_active:
+                key = (lane, direction)
+                if key in lane_data:
+                    lane_df = lane_data[key]
+                    counts = lane_df.groupby('hour_bin')['count'].sum()
+
+                    ax.plot(counts.index, counts, label=f'{direction.capitalize()} (Lane {lane})')
+                    total_data = total_data.add(counts, fill_value=0)
+
+        # Ensure total_data covers all hour_bins by filling missing hours with the sum of available data
+        total_data = total_data.reindex(hour_bins, fill_value=0)
+
+        # Plot the total data after processing all lanes
+        ax.plot(total_data.index, total_data, color='red', label='Total', linestyle='--')
+
+        start_date, end_date = self.date_range
+        ax.set_title(f'{title} Vehicles by Hour\n({start_date} to {end_date})')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Number of Vehicles')
+        ax.grid(True)
+        ax.set_xticks(hour_bins)
+        ax.set_xticklabels(x_ticks, rotation=45)
+        ax.legend()
+
+        max_y_value = total_data.max()
+        fig_width, fig_height = self.fig.get_size_inches()
+        grid_interval = max(100, int(max_y_value / 10))
+        max_y_limit = (int(max_y_value // grid_interval) + 1) * grid_interval
+
+        ax.set_ylim(0, max_y_limit)
+        ax.set_yticks(range(0, max_y_limit + 1, grid_interval))
 
     def on_close(self):
         self.atm.vds_view = False
         self.root.destroy()
+
 
 def start_vds_view_thread(config, atm, start_date, day):
     result_queue = queue.Queue()
@@ -204,26 +211,28 @@ def start_vds_view_thread(config, atm, start_date, day):
     data_thread.start()
     plot_app_thread.start()
 
+
 def run_vds_view(config, selected_atms, start_date, day):
     for atm in selected_atms:
         start_vds_view_thread(config, atm, start_date, day)
+
 
 def start_vds_view(config, selected_atms):
     date, day = open_input_dialog()
     vds_thread = threading.Thread(target=run_vds_view, args=(config, selected_atms, date, day))
     vds_thread.start()
 
+
 def open_input_dialog():
     root = tk.Tk()
-    root.withdraw()  # Tkinter 메인 창 숨기기
+    root.withdraw()
 
-    # 날짜 입력 받기
     date = simpledialog.askstring("Input", "Enter the date (YYMMDD):")
-    
     day = simpledialog.askstring("Input", "Enter the period (number of days):")
 
-    root.destroy()  # Tkinter 창 닫기
+    root.destroy()  # Close the Tkinter input dialog
     return date, day
+
 
 def capitalize_first_letter(string):
     if len(string) == 0:
